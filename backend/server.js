7f0fcfe63ffd111f7e696c3d968e723ba08eb0a7
AndreJSON <andre.v.json@gmail.com>
2017-03-03 18:40:20 +0100

    Adds code from a template I made during an earlier lab.

---
+++ backend/server.js
@@ -0,0 +1,16 @@
+/*jslint node: true, nomen: true, es5: true*/
+'use strict';
+var port = 8080;
+var express = require('express');
+var app = express();
+var chalk = require('chalk');
+
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+
+var router = require('./routes.js');
+app.use('/api', router);
+require('./paths.js')(app);
+
+app.listen(port);
+console.log(chalk.green("Server is now listening on port: ") + chalk.magenta(port));
