b02d693751a270e6fa3b6e8626ea34ec9ea862f1
AndreJSON <andre.v.json@gmail.com>
2017-03-05 12:24:04 +0100

    Adds model file.

---
+++ backend/model.js
@@ -0,0 +1,64 @@
+var Sequelize = require('sequelize');
+
+module.exports = function (seq) {
+	var model = {};
+
+	model.Users = seq.define('users', {
+		id: {
+			type: Sequelize.INTEGER,
+			autoIncrement: true,
+			primaryKey: true
+		},
+		name: {
+			type: Sequelize.STRING
+		}.
+		hash: {
+			type: Sequelize.STRING
+		},
+		salt: {
+			type: Sequelize.STRING
+		}
+	});
+
+	model.Recipes = seq.define('recipes', {
+		id: {
+			type: Sequelize.INTEGER,
+			autoIncrement: true,
+			primaryKey: true
+		},
+		authorId: {
+			type: Sequelize.INTEGER
+		},
+		name: {
+			type: Sequelize.STRING
+		},
+		image: {
+			type: Sequelize.DOUBLE
+		},
+		freetext: {
+			type: Sequelize.INTEGER
+		},
+		tags: {
+			type: Sequelize.ARRAY(Sequelize.INTEGER);
+		}
+	});
+
+	model.Comments = seq.define('trades', {
+		id: {
+			type: Sequelize.INTEGER,
+			autoIncrement: true,
+			primaryKey: true
+		},
+		recipeId: {
+			type: Sequelize.INTEGER
+		},
+		authorId: {
+			type: Sequelize.INTEGER
+		},
+		freetext: {
+			type: Sequelize.STRING
+		}
+	});
+
+	return model;
+}
