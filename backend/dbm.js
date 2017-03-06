f532da61e876aa475aef28241ceb3a79a640e8e2
AndreJSON <andre.v.json@gmail.com>
2017-03-06 10:57:49 +0100

    Adds some more functions for performing queries.

--- backend/dbm.js
+++ backend/dbm.js
@@ -16,11 +16,31 @@ var seq = new Sequelize('rfd', 'postgres', null, {
 var model = require('./model.js')(seq); //Create the models.
 
 exports.addRecipe = function (authorId, name, image, freetext, tags) {
-	return model.Users.create({
+	return model.Recipes.create({
 		authorId: authorId,
 		name: name,
 		image: image,
 		freetext: freetext,
 		tags: tags
+	});
+};
+
+exports.addComment = function (recipeId, authorId, freetext) {
+	return model.Comments.create({
+		recipeId: recipeId,
+		authorId: authorId,
+		freetext: freetext
+	});
+};
+
+exports.getRecipes = function () {
+	return model.Recipes.findAll();
+};
+
+exports.getComments = function (recipeId) {
+	return model.Comments.findAll({
+		where: {
+			recipeId: recipeId
+		}
 	});
 };
